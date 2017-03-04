e5b08b93b72e65241d5240fd657db0172d7dab35
Lukas <seriouz.accz@gmail.com>
2017-03-04 21:52:08 +0100

    Add ci & deployment code

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+script_dir=$(dirname $0)
+
+if [ "$#" -ne 1 ]; then
+    echo "Exactly one argument is required (location of artifacts dir)"
+    exit 1
+fi
+
+echo "Deploying unreal-scrapper via ansible. Artifacts dir is $1"
+cd "$script_dir/deployment"
+ansible-playbook "deploy.yml" --extra-vars="artifact_path=$1"
